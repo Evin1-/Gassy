531299f8d7e2e93c1f932a6c5d2d117b0fe5ee68
Evin1- <edwin.hdz.12.0@gmail.com>
2016-06-26 16:55:18 -0400

    Add StationsFragment logic

--- app/src/main/java/com/loopcupcakes/gassy/entities/firebase/User.java
+++ app/src/main/java/com/loopcupcakes/gassy/entities/firebase/User.java
@@ -4,6 +4,7 @@ package com.loopcupcakes.gassy.entities.firebase;
  * Created by evin on 6/20/16.
  */
 public class User {
+
     public String email;
 
     public User() {
--- app/src/main/java/com/loopcupcakes/gassy/fragments/StationsFragment.java
+++ app/src/main/java/com/loopcupcakes/gassy/fragments/StationsFragment.java
@@ -2,29 +2,117 @@ package com.loopcupcakes.gassy.fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.loopcupcakes.gassy.R;
+import com.loopcupcakes.gassy.adapters.StationsAdapter;
+import com.loopcupcakes.gassy.entities.firebase.Station;
+import com.loopcupcakes.gassy.entities.places.Location;
+import com.loopcupcakes.gassy.entities.places.PlaceResponse;
+import com.loopcupcakes.gassy.entities.places.Result;
+import com.loopcupcakes.gassy.network.RetrofitHelper;
+import com.loopcupcakes.gassy.util.NetworkChecker;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class StationsFragment extends Fragment {
 
+    private static final String TAG = "StationsFragmentTAG_";
+
+    private RecyclerView mRecyclerView;
+    private ArrayList<Result> mResults;
+    private StationsAdapter mAdapter;
+
+    private DatabaseReference mDatabase;
+
+    private static final String LATITUDE_TEST = "19.3352665";
+    private static final String LONGITUDE_TEST = "-99.172322";
 
     public StationsFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setRetainInstance(true);
+        mResults = new ArrayList<>();
+        mAdapter = new StationsAdapter(mResults);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_stations, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.f_stations_recycler);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
+
+        refreshRecycler();
+    }
+
+    private void refreshRecycler() {
+        RetrofitHelper retrofitHelper = new RetrofitHelper();
+        Call<PlaceResponse> placeResponseCall = retrofitHelper.buildCall(LATITUDE_TEST, LONGITUDE_TEST);
+        if (NetworkChecker.checkInternet(getActivity().getApplicationContext()) != NetworkChecker.NO_CONNECTION) {
+            placeResponseCall.enqueue(new Callback<PlaceResponse>() {
+                @Override
+                public void onResponse(Call<PlaceResponse> call, Response<PlaceResponse> response) {
+                    mResults.clear();
+                    mResults.addAll(response.body().getResults());
+                    mAdapter.notifyDataSetChanged();
+
+                    for (Result result : mResults) {
+                        Log.d(TAG, "onResponse: " + result.getGeometry().getLocation() + " " + result.getName());
+                        pushStation(result);
+                    }
+
+                }
+
+                @Override
+                public void onFailure(Call<PlaceResponse> call, Throwable t) {
+
+                }
+            });
+        } else {
+            // TODO: 6/3/16 Print SnackBar in correct view
+            Snackbar.make(mRecyclerView, "No Internet connection", Snackbar.LENGTH_LONG).show();
+        }
+    }
+
+    private void pushStation(Result result) {
+        Location location = result.getGeometry().getLocation();
+        Double latitude = location.getLat();
+        Double longitude = location.getLng();
+        String placeId = result.getPlaceId();
+
+        Station station = new Station(latitude, longitude, 0.0, true);
+        mDatabase.child("stations").child(placeId).setValue(station);
     }
 
 }
