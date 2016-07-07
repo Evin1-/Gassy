2fdfd098c1aeee76a7b523a6584e6a996e891eaf
Evin1- <edwin.hdz.12.0@gmail.com>
2016-07-06 23:27:41 -0400

    Add LocationHelper

---
+++ app/src/main/java/com/loopcupcakes/gassy/util/LocationHelper.java
@@ -0,0 +1,68 @@
+package com.loopcupcakes.gassy.util;
+
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * Created by evin on 6/28/16.
+ */
+public class LocationHelper implements LocationListener {
+    private static final String TAG = "GPSHelperTAG_";
+
+    private LocationManager mLocationManager;
+
+    public LocationHelper(Context context) {
+        mLocationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d(TAG, "onLocationChanged: " + location.getAccuracy());
+        if (location.getAccuracy() < 100 && mLocationManager != null) {
+            mLocationManager.removeUpdates(this);
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+        Log.d(TAG, "onStatusChanged: ");
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+        Log.d(TAG, "onProviderEnabled: ");
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        Log.d(TAG, "onProviderDisabled: ");
+    }
+
+    public void requestLocationUpdate() {
+        Log.d(TAG, "requestLocationUpdate: ");
+        if (mLocationManager != null) {
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+            mLocationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
+        }
+    }
+
+    public void stopLocationUpdate() {
+        if (mLocationManager != null) {
+            mLocationManager.removeUpdates(this);
+        }
+    }
+
+    public Location getLastLocation() {
+        if (mLocationManager != null) {
+            Criteria criteria = new Criteria();
+            String bestProvider = mLocationManager.getBestProvider(criteria, false);
+            return mLocationManager.getLastKnownLocation(bestProvider);
+        }
+        return null;
+    }
+}
