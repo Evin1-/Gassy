e5a76f6a0e9155a6b4734ad47d14dce4f4fbb70d
Evin1- <edwin.hdz.12.0@gmail.com>
2016-07-07 00:47:29 -0400

    Add Application class with LeakCanary initialization

---
+++ app/src/main/java/com/loopcupcakes/gassy/App.java
@@ -0,0 +1,16 @@
+package com.loopcupcakes.gassy;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * Created by evin on 7/7/16.
+ */
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        LeakCanary.install(this);
+    }
+}
